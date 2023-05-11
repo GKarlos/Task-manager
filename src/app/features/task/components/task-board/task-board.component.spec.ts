2615717df23ed7a423421d0ef8c79f373e37710b
GKarlos <krloes777@gmail.com>
2023-05-11 00:27:45 -0300

    feature(task): delegate the task of openinig the dialog to the task board component

--- src/app/features/task/components/task-board/task-board.component.spec.ts
+++ src/app/features/task/components/task-board/task-board.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
 import { TaskBoardComponent } from './task-board.component';
 import { Task } from '@task/models/task';
 import { TEST_TASK } from '@task/constants/task-testing.constants';
@@ -7,17 +10,19 @@ import { TaskModule } from '@task/task.module';
 describe('TaskBoardComponent', () => {
   let component: TaskBoardComponent;
   let fixture: ComponentFixture<TaskBoardComponent>;
+  let dialog: MatDialog;
   let testTaskList: Task[];
   let testTask: Task;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TaskModule],
+      imports: [TaskModule, BrowserAnimationsModule],
       declarations: [TaskBoardComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TaskBoardComponent);
     component = fixture.componentInstance;
+    dialog = TestBed.inject(MatDialog);
     testTask = TEST_TASK;
     testTaskList = [
       { ...testTask, id: '1' },
@@ -101,5 +106,42 @@ describe('TaskBoardComponent', () => {
 
     expect(component.tasks.length).toBe(3);
     expect(component.tasks).toContain(newTask);
+  });
+  it('should call openTaskDialog with the correct parameters for adding and editing tasks', () => {
+    spyOn(component, 'openTaskDialog').and.callFake((task?: Task) => {
+      const dialogRefSpy = jasmine.createSpyObj('MatDialogRef', [
+        'afterClosed',
+        'close',
+      ]);
+      dialogRefSpy.afterClosed.and.returnValue(of(task));
+      return dialogRefSpy;
+    });
+
+    component.openTaskDialog(undefined);
+    expect(component.openTaskDialog).toHaveBeenCalledWith(undefined);
+
+    component.openTaskDialog(testTask);
+    expect(component.openTaskDialog).toHaveBeenCalledWith(testTask);
+  });
+
+  it('should handle the dialog result correctly for adding and editing tasks', () => {
+    spyOn(component, 'handleAddTask');
+    spyOn(component, 'handleEditTask');
+
+    const dialogRefSpy = jasmine.createSpyObj('MatDialogRef', [
+      'afterClosed',
+      'close',
+    ]);
+    spyOn(dialog, 'open').and.returnValue(dialogRefSpy);
+
+    dialogRefSpy.afterClosed.and.returnValue(of(testTask));
+    component.openTaskDialog();
+    dialogRefSpy.close();
+    expect(component.handleAddTask).toHaveBeenCalledWith(testTask);
+
+    dialogRefSpy.afterClosed.and.returnValue(of(testTask));
+    component.openTaskDialog(testTask);
+    dialogRefSpy.close();
+    expect(component.handleEditTask).toHaveBeenCalledWith(testTask);
   });
 });
--- src/app/features/task/components/task-list/task-list.component.ts
+++ src/app/features/task/components/task-list/task-list.component.ts
@@ -1,8 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
 import { Task } from '@task/models/task';
-import { TaskFormComponent } from '@task/components/task-form/task-form.component';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-task-list',
@@ -11,23 +8,8 @@ import { Subscription } from 'rxjs';
 })
 export class TaskListComponent {
   @Input() taskList: Task[] = [];
-  private dialogRefSubscription: Subscription | undefined;
   @Output() deleteTask = new EventEmitter<Task>();
-  @Output() addTask = new EventEmitter<Task>();
-
-  constructor(public dialog: MatDialog) {}
-
-  onAddTask() {
-    if (this.dialogRefSubscription) {
-      this.dialogRefSubscription.unsubscribe();
-    }
-    const dialogRef = this.dialog.open(TaskFormComponent);
-    this.dialogRefSubscription = dialogRef
-      .afterClosed()
-      .subscribe((newTask: Task | undefined) => {
-        if (newTask) this.addTask.emit(newTask);
-      });
-  }
+  @Output() openAddDialog = new EventEmitter<void>();
 
   onDeleteTask(task: Task) {
     this.deleteTask.emit(task);
