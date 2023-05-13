d1e23cf8322f57c37d0f72fe0562eab5f3eb00df
GKarlos <krloes777@gmail.com>
2023-05-12 22:12:45 -0300

    refactor(task-form): remove unneeded component

--- src/app/features/task/task.module.ts
+++ src/app/features/task/task.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TaskRoutingModule } from './task-routing.module';
-import { TaskListComponent } from './components/task-list/task-list.component';
-import { TaskItemComponent } from './components/task-item/task-item.component';
-import { TaskFormComponent } from './components/task-form/task-form.component';
-import { ContainerTaskListComponent } from './containers/container-task/container-task-list.component';
+import { TaskRoutingModule } from '@task/task-routing.module';
+import { TaskListComponent } from '@task/components/task-list/task-list.component';
+import { TaskItemComponent } from '@task/components/task-item/task-item.component';
+import { TaskBoardComponent } from '@task/components/task-board/task-board.component';
+import { TaskDetailsComponent } from '@task/components/task-details/task-details.component';
+import { ContainerTaskListComponent } from '@task/containers/container-task/container-task-list.component';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -12,14 +13,11 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { SharedModule } from '@shared/shared.module';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { TaskDetailsComponent } from './components/task-details/task-details.component';
-import { TaskBoardComponent } from './components/task-board/task-board.component';
 
 @NgModule({
   declarations: [
     TaskListComponent,
     TaskItemComponent,
-    TaskFormComponent,
     ContainerTaskListComponent,
     TaskDetailsComponent,
     TaskBoardComponent,
