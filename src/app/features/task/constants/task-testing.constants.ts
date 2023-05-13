633823d0b1e0bf1adc0bfff6433c396910b1d73f
GKarlos <krloes777@gmail.com>
2023-05-12 22:12:45 -0300

    refactor(task-form): remove unneeded component

--- src/app/features/task/constants/task-testing.constants.ts
+++
@@ -1,9 +0,0 @@
-import { Task } from '@task/models/task';
-
-export const TEST_TASK: Task = {
-  title: 'Test task title',
-  description: 'Test task description',
-  id: '123',
-  isComplete: false,
-  isExpanded: false,
-};
