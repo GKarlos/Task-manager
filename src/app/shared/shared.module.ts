734049de9619f5c6baeb494b161fde7d84d13d7e
GKarlos <krloes777@gmail.com>
2023-05-16 19:21:16 -0300

    refactor: correct typo

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import { EventBarComponent } from '@shared/components/title-bar/event-bar.component';
+import { EventBarComponent } from '@shared/components/event-bar/event-bar.component';
 
 @NgModule({
   declarations: [EventBarComponent],
